07732609a7f5ea38082b278ec2e65e5634aa1b2d
lawsberryPi <shawn-yu@huskers.unl.edu>
2019-05-09 22:32:18 -0400

    added test module

---
+++ XUnitTestShawnCodeChanllenge/TestMain.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using ReactReduxIntergration.Controllers;
+
+namespace XUnitTestShawnCodeChanllenge
+{
+    public class TestMain
+    {
+        [Fact]
+        public void Test1()
+        {
+            LinksController linkControllerTester = new LinksController();
+        }
+    }
+}
